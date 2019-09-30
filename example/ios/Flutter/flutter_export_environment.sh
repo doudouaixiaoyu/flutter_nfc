11908c7fe1a7e2f9b073bb231cd3e52052eb4f36
gehaoran <1074045339@qq.com>
2019-09-30 15:08:21 +0800

    升级flutter 版本

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gehaoran/Project/FlutterPlugin/flutter_nfc/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Applications/flutter/bin/cache/artifacts/engine/ios"
